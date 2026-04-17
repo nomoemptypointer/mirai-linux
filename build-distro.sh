6365308939adc3f5b693cd79dc0d0c2bb38f977e
nomo <51972322+nomoemptypointer@users.noreply.github.com>
2026-04-17 17:54:27 +0200

    Make networking and users work

--- build-distro.sh
+++ build-distro.sh
@@ -142,6 +142,52 @@ cd builds-cache/initramfs
     ln -s /lib64/libc.so.6 lib/libc.so.6
 cd ../..
 
+# Set hosts
+# 127.0.0.1 is also hostname but idk if it should be "mirai" here too
+cat > builds-cache/initramfs/etc/hosts << EOF
+127.0.0.1  localhost
+::1        localhost
+EOF
+
+# Set up all the default users
+cat > builds-cache/initramfs/etc/passwd << "EOF"
+root:x:0:0:root:/root:/bin/sh
+bin:x:1:1:bin:/dev/null:/usr/bin/false
+daemon:x:6:6:Daemon User:/dev/null:/usr/bin/false
+messagebus:x:18:18:D-Bus Message Daemon User:/run/dbus:/usr/bin/false
+uuidd:x:80:80:UUID Generation Daemon User:/dev/null:/usr/bin/false
+nobody:x:65534:65534:Unprivileged User:/dev/null:/usr/bin/false
+EOF
+
+# Set up all the default groups
+cat > builds-cache/initramfs/etc/group << "EOF"
+root:x:0:
+bin:x:1:daemon
+sys:x:2:
+kmem:x:3:
+tape:x:4:
+tty:x:5:
+daemon:x:6:
+floppy:x:7:
+disk:x:8:
+lp:x:9:
+dialout:x:10:
+audio:x:11:
+video:x:12:
+utmp:x:13:
+usb:x:14:
+cdrom:x:15:
+adm:x:16:
+messagebus:x:18:
+input:x:24:
+mail:x:34:
+kvm:x:61:
+uuidd:x:80:
+wheel:x:97:
+users:x:999:
+nogroup:x:65534:
+EOF
+
 # Set cloudflare as default DNS server
 cat > builds-cache/initramfs/etc/resolv.conf << EOF
 nameserver 1.1.1.1
